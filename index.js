1e12796e802a2d17d36e773dc60e64cb5f0e6743
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-11-16 23:37:24 +0530

    addd

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const fs = require('fs');
-const replaceThis = "file"
-const replaceWith = "john"
-const preview = true
+const fs = require("fs");
+const replaceThis = "file";
+const replaceWith = "john";
+const preview = true;
 
 try {
-fs.readdir(".", (err, data)=>{
+  fs.readdir(".", (err, data) => {
     for (let index = 0; index < data.length; index++) {
-        const item = data[index];
-        let newFile = "data/" + item.replaceAll(replaceThis, replaceWith)
-        if (!preview) {
-            fs.rename("data/" + item, newFile, ()=>{
-                console.log("Rename Success")
-            })
+      const item = data[index];
+      let newFile = "data/" + item.replaceAll(replaceThis, replaceWith);
+      if (!preview) {
+        fs.rename("data/" + item, newFile, () => {
+          console.log("Rename Success");
+        });
+      } else {
+        if ("data/" + item !== newFile) {
+          console.log("data/" + item + " will be renamed to " + newFile);
         }
-        else{
-          if ("data/" + item !== newFile) {
-            console.log("data/" + item + " will be renamed to " + newFile)
-          }
-        }
+      }
     }
-})
+  });
 } catch (err) {
   console.error(err);
 }
