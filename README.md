04a8a8677dbf43b631aca3c31ac15d1a3c648d39
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-11-24 02:19:20 +0530

    Complete the Project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FileRenamer
+JavaScript Project
+To change the File name of bulk file 
+Credit to Code with Harry
--- rename.sh
+++ rename.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+script_name=$0
+script_full_path=$(dirname "$0")
+
+echo "script_name: $script_name"
+echo "full path: $script_full_path"
 node index.js
